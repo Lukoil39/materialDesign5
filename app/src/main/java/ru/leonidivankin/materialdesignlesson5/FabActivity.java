5035ca160a26cfa5c5a7d533c31e63bc88a6f92f
LeonidIvankin <lim14@yandex.ru>
2019-07-22 23:33:43 +0300

    migrate androidx

--- app/src/main/java/ru/leonidivankin/materialdesignlesson5/FabActivity.java
+++ app/src/main/java/ru/leonidivankin/materialdesignlesson5/FabActivity.java
@@ -1,10 +1,8 @@
 package ru.leonidivankin.materialdesignlesson5;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class FabActivity extends AppCompatActivity {
 
